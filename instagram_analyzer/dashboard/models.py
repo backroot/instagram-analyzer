89cf7aba337fb3cd5e8d50773ede3fdf0bcb12bf
backroot <kotaro_saito@backroot.net>
2019-10-13 17:58:40 +0900

    add table for account, follower, follow

--- instagram_analyzer/dashboard/models.py
+++ instagram_analyzer/dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class account(models.Model):
+    user_id      = models.IntegerField()
+    account_name = models.CharField(max_length=100)
+    create_date  = models.DateTimeField(auto_now_add=True)
+    update_date  = models.DateTimeField(auto_now=True)
+
+class follower(models.Model):
+    account_id  = models.IntegerField()
+    scrape_date = models.DateField()
+    followers   = models.IntegerField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+class follow(models.Model):
+    account_id  = models.IntegerField()
+    scrape_date = models.DateField()
+    follows     = models.IntegerField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
