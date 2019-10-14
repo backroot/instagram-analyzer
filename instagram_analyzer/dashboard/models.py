91365bcc36a653abc3094a6e9492e90fe8c13eee
backroot <kotaro_saito@backroot.net>
2019-10-14 09:39:25 +0900

    fix table layout

--- instagram_analyzer/dashboard/models.py
+++ instagram_analyzer/dashboard/models.py
@@ -6,22 +6,20 @@ class user(models.Model):
     create_date  = models.DateTimeField(auto_now_add=True)
     update_date  = models.DateTimeField(auto_now=True)
 
-class account(models.Model):
+class account_summary(models.Model):
     user_id      = models.IntegerField()
     account_name = models.CharField(max_length=100)
+    scrape_date  = models.DateField()
+    posts        = models.IntegerField()
+    followers    = models.IntegerField()
+    follows      = models.IntegerField()
     create_date  = models.DateTimeField(auto_now_add=True)
     update_date  = models.DateTimeField(auto_now=True)
 
-class follower(models.Model):
-    account_id  = models.IntegerField()
-    scrape_date = models.DateField()
-    followers   = models.IntegerField()
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-
-class follow(models.Model):
-    account_id  = models.IntegerField()
-    scrape_date = models.DateField()
-    follows     = models.IntegerField()
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
+    class Meta:
+            unique_together = [['user_id', 'account_name', 'scrape_date']]
+            indexes = [
+                models.Index(fields=['posts']),
+                models.Index(fields=['followers']),
+                models.Index(fields=['follows'])
+            ]
