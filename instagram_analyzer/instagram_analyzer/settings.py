dce98e60966db2e7860eb727a00f60c19044fd54
backroot <kotaro_saito@backroot.net>
2019-10-21 03:05:43 +0900

    debug off

--- instagram_analyzer/instagram_analyzer/settings.py
+++ instagram_analyzer/instagram_analyzer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eefy(17$kz04luw1ef1fs3+t+sez%q2z=dq=1)&-z-&o3ej^*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
